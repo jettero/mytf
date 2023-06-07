47c096567678a2241aa5e425e1f1202ecc2eacf9
Paul Miller <paul@jettero.pl>
2023-06-07 13:39:52 -0400

    goom

--- tf_imp/__init__.py
+++ tf_imp/__init__.py
@@ -7,8 +7,4 @@ stfu_tensorflow()
 
 from .strings import *
 
-# there's actually not much to import here mostly we're invoking the egglib
-# __init__.py what adds all the eggs to the import path.
-from .egglib import *
-
 from .grid_world import run_check as gw_run_check, GridWorld, Room, A as GWActions
