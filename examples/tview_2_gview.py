1d652d0b4aaeba93ab53dcaf61676712f260b2a1
Paul Miller <paul@jettero.pl>
2023-07-02 19:21:52 -0400

    ok, now this is neat. I'm overtraining for sure, but it's working.

--- examples/tview_2_gview.py
+++ examples/tview_2_gview.py
@@ -6,6 +6,7 @@ import mytf
 import tensorflow as tf
 import numpy as np
 
+from mytf.grid_world.const import GOAL
 from mytf.grid_world.agent import Agent
 from mytf.grid_world.util import decode_view
 from mytf.strings import side_by_side
@@ -34,56 +35,58 @@ for a in actions:
     y_true = np.concatenate((y_true, agent.one_hot_shortrange_goal().reshape(ys)))
 
 x_data = features_last(x_data)
-y_true = features_last(y_true)
+y_true = y_true[:, GOAL]
 
 print(f"x_data = {x_data.shape}")
 print(f"y_true = {y_true.shape}")
 
 N = np.prod(x_data.shape[1:])
 M = np.prod(y_true.shape[1:])
-I = tf.keras.layers.Input(shape=x_data.shape[1:], name='left of bang')
+I = tf.keras.layers.Input(shape=x_data.shape[1:], name="left of bang")
 
 o = I
-o = tf.keras.layers.Conv2D(kernel_size=3, filters=4*N, activation='relu')(o)
-o = tf.keras.layers.Dense(4*N, activation='relu')(o)
+o = tf.keras.layers.Conv2D(kernel_size=3, filters=4 * N, activation="relu")(o)
+o = tf.keras.layers.Dense(4 * N, activation="relu")(o)
 o = tf.keras.layers.Dropout(0.1)(o)
-o = tf.keras.layers.Dense(4*N, activation='relu')(o)
-o = tf.keras.layers.Conv2DTranspose(kernel_size=3, filters=4*N, activation='relu')(o)
-print(f'thought: {o.shape}')
+o = tf.keras.layers.Dense(4 * N, activation="relu")(o)
+o = tf.keras.layers.Conv2DTranspose(kernel_size=3, filters=4 * N, activation="relu")(o)
+print(f"thought: {o.shape}")
 
-o = tf.keras.layers.Dense(M, activation='relu')(o)
+o = tf.keras.layers.Dense(M, activation="relu")(o)
 o = tf.keras.layers.Dropout(0.1)(o)
-o = tf.keras.layers.Dense(M, activation='relu')(o)
-o = tf.keras.layers.Dense(y_true.shape[-1], activation='relu')(o)
-print(f'final: {o.shape}')
+o = tf.keras.layers.Dense(M, activation="sigmoid")(o)
+o = tf.keras.layers.Conv2DTranspose(kernel_size=1, filters=1, activation='sigmoid')(o)
+o = tf.keras.layers.Reshape(y_true.shape[1:])(o)
+print(f"final: {o.shape}")
 
-SenseGoal = tf.keras.Model(inputs=(I,), outputs=(o,), name='SenseGoal')
-SenseGoal.compile(loss='mse', optimizer='adam')
+SenseGoal = tf.keras.Model(inputs=(I,), outputs=(o,), name="SenseGoal")
+SenseGoal.compile(loss="mse", optimizer="adam")
 SenseGoal.summary()
 
-y_pred, = SenseGoal(x_data)
-print(f'y_pred: {y_pred.shape}')
+(y_pred,) = SenseGoal(x_data)
+print(f"y_pred: {y_pred.shape}")
 
-def do_lap(epochs=100, workers=NUM_CPU):
+
+def do_lap(epochs=100, workers=NUM_CPU, skip_print=False):
     global y_pred, ypff
     SenseGoal.fit(x=x_data, y=y_true, epochs=epochs, verbose=True, use_multiprocessing=True, workers=workers)
-    y_pred, = SenseGoal(x_data)
-    for t,p in zip(y_true, y_pred):
-        dt = decode_view(features_first(t), with_turtle=True)
-        dp = decode_view(features_first(p), with_turtle=True)
-        print( side_by_side(str(dt), str(dp)) )
-    if IPYTHON:
-        ypff = features_first(y_pred)
+    (y_pred,) = SenseGoal(x_data)
+    if not skip_print:
+        for x, y in zip(x_data, y_pred):
+            dx = decode_view(features_first(x), with_turtle=True)
+            dy = str(tf.round(100 * y) / 100)
+            print(side_by_side(str(dx), str(dy)))
 
-if __name__ == '__main__' and not IPYTHON:
+
+if __name__ == "__main__" and not IPYTHON:
     while True:
         try:
-            epochs = int(input('epochs (0: exit): '))
+            epochs = int(input("epochs (0: exit): "))
             if epochs < 1:
                 break
         except:
             continue
         do_lap(epochs=epochs)
 else:
-    do_lap(epochs=5)
+    do_lap(epochs=1, skip_print=True)
     print("use /do_lap [epochs=100] to run the network, populate ypff, and print things")
