919fe14ad262163a05f875dcc2d01d67f6d76ab2
Paul Miller <paul@jettero.pl>
2023-07-05 12:02:52 -0400

    this shit from last night doesn't work... not sure why

--- examples/tv2gv.py
+++ examples/tv2gv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import sys, os
 import multiprocessing
 import mytf
 import tensorflow as tf
@@ -29,16 +30,14 @@ def compute_xy(*worlds):
         xs = (1, *x0.shape)
         ys = (1, *y0.shape)
         x_data = x0.reshape(xs)
-        y_true = y0.reshape(ys)
 
         actions = agent.shortest_path_to_goal()
         for a in actions:
             agent.do_move(a)
             x_data = np.concatenate((x_data, gw.tview.reshape(xs)))
-            y_true = np.concatenate((y_true, agent.one_hot_shortrange_goal().reshape(ys)))
 
         x_data = features_last(x_data)
-        y_true = y_true[:, GOAL]
+        y_true = agent.sps
 
         yield NumpyTuple(x_data, y_true, nb_depth=3)
 
@@ -48,55 +47,72 @@ data = list(compute_xy(
     mytf.grid_world.SuperHardRoom(),
 ))
 
-max_group_size = max( x.shape[0] for x,y in data )
+# max_group_size = max( x.shape[0] for x,y in data )
+# print(f'max_group_size = {max_group_size}')
 
-print(f'max_group_size = {max_group_size}')
+x_data = tf.ragged.stack([ i[0] for i in data ], axis=0).to_tensor()
+y_true = tf.ragged.stack([ i[1] for i in data ], axis=0).to_tensor()
 
-import sys
-sys.exit(0)
+x_shape = x_data.shape[1:]
+y_shape = y_true.shape[1:]
 
+N = np.prod(x_shape[1:]) # skip the steps dimension giving 7,7,5
+M = np.prod(y_shape) # 15,17
+
+print(f'x_shape: {x_shape}')
+print(f'y_shape: {y_shape}')
 print(f'x_data: {x_data.shape}')
 print(f'y_true: {y_true.shape}')
-
-N = np.prod(x_data.shape[1:])
-M = np.prod(y_true.shape[1:])
-
 print(f'\nN={N}; M={M}')
 
-I = tf.keras.layers.Input(shape=x_data.shape[1:], name="left of bang")
+def exit_if_true(x):
+    try:
+        if int(os.environ[x]) > 0:
+            sys.exit(0)
+    except (KeyError, TypeError):
+        pass
+
+exit_if_true('DATA_ONLY')
+
+I = tf.keras.layers.Input(shape=x_shape, name="left of bang")
 
 o = I
-o = tf.keras.layers.Conv2D(kernel_size=3, filters=4 * N, activation="relu")(o)
-o = tf.keras.layers.Dense(4 * N, activation="relu")(o)
-o = tf.keras.layers.Dropout(0.1)(o)
-o = tf.keras.layers.Dense(4 * N, activation="relu")(o)
-o = tf.keras.layers.Conv2DTranspose(kernel_size=3, filters=4 * N, activation="relu")(o)
+o = tf.keras.layers.ConvLSTM2D(kernel_size=3, filters=4*N, activation="relu")(o)
 print(f"thought: {o.shape}")
+exit_if_true('THOUGHT_ONLY')
 
-o = tf.keras.layers.Dense(M, activation="relu")(o)
+while np.prod(o.shape[1:3]) < M:
+    s0 = o.shape[1:]
+    o = tf.keras.layers.Conv2DTranspose(kernel_size=3, filters=4*M, activation='relu')(o)
+    print(f'{s0} -> {o.shape[1:]}')
+
+o = tf.keras.layers.Flatten()(o)
+o = tf.keras.layers.Dense(M, activation='sigmoid')(o)
 o = tf.keras.layers.Dropout(0.1)(o)
-o = tf.keras.layers.Dense(M, activation="sigmoid")(o)
-o = tf.keras.layers.Conv2DTranspose(kernel_size=1, filters=1, activation='sigmoid')(o)
-o = tf.keras.layers.Reshape(y_true.shape[1:])(o)
-print(f"final: {o.shape}")
+o = tf.keras.layers.Reshape(y_shape)(o)
+print(f"final-reshape: {o.shape}")
+exit_if_true('FINAL_ONLY')
 
 SenseGoal = tf.keras.Model(inputs=(I,), outputs=(o,), name="SenseGoal")
 SenseGoal.compile(loss="mse", optimizer="adam")
 SenseGoal.summary()
 
+exit_if_true('SUMMARY_ONLY')
+
 (y_pred,) = SenseGoal(x_data)
 print(f"y_pred: {y_pred.shape}")
 
+exit_if_true('PRED_ONLY')
 
 def do_lap(epochs=100, workers=NUM_CPU, skip_print=False):
     global y_pred, ypff
     SenseGoal.fit(x=x_data, y=y_true, epochs=epochs, verbose=True, use_multiprocessing=True, workers=workers)
     (y_pred,) = SenseGoal(x_data)
     if not skip_print:
-        for x, y in zip(x_data, y_pred):
-            dx = decode_view(features_first(x), with_turtle=True)
-            dy = str(tf.round(100 * y) / 100)
-            print(side_by_side(str(dx), str(dy)))
+        for t, p in zip(y_true, y_pred):
+            dt = str(np.array(tf.round(100 * t) / 10, dtype=np.int32))
+            dp = str(np.array(tf.round(100 * p) / 10, dtype=np.int32))
+            print(side_by_side(str(dt), str(dp)))
 
 
 if __name__ == "__main__" and not IPYTHON:
